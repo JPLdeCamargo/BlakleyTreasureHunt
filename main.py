109a7e2242418073e2d46dfa473d76fd0fefb5bf
Vitor Hugo Homem Marzarotto <vitormarzarotto@hotmail.com>
2024-07-01 12:36:17 -0300

    verificação de comprimento

--- main.py
+++ main.py
@@ -8,8 +8,17 @@ def convert_int_to_utf8(num ):
     binary_array = binary_int.to_bytes(byte_number, "big")
     return binary_array.decode()
 
-segredo = "pastel"
+segredo = "pastel pastel pastel pastel pastel a"
 p = 5217712569307149314884997411507616511961101226671352088041037360428537519575582495381
+secret_bytes = segredo.encode("utf-8")
+secretInt = int.from_bytes(secret_bytes)
+
+len_segredo =len(bin(secretInt))
+len_p = len(bin(p))
+if (len_segredo > len_p) :
+    raise Exception("Segredo mt grande para o valor de P")
+
+
 blakley = Blakley(2,2,p)
 
 generateQrCodesFromSecret(segredo,blakley)
